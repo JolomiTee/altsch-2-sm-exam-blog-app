e87c1132b4d3ee6b8a419c615266ea21e45a4580
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-06-12 23:45:30 +0100

    - error fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { connectToMongoDB } = require("./config/connectDb");
 
 const HOST = process.env.HOST || 3000;
 
-app.listen(PORT, () => {
+app.listen(HOST, () => {
 	connectToMongoDB();
 	console.log(`Server active on port ${HOST}`);
 });
