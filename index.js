ae19f1d36a629d01d1aef49df40cda2b11e382da
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-06-12 22:58:07 +0100

    adjusted host port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const { connectToMongoDB } = require("./config/connectDb");
 
-const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 3000;
 
 app.listen(PORT, () => {
 	connectToMongoDB();
-	console.log(`Server active on port ${PORT}`);
+	console.log(`Server active on port ${HOST}`);
 });
